98fbbc37853fffab0f1016ad2e528f35de214b36
shimashima <shimashima@pixiv.com>
2017-12-04 11:12:55 +0900

    不要部分の削除

--- src/test/java/jp/dip/prog/TestBase.java
+++ src/test/java/jp/dip/prog/TestBase.java
@@ -1,10 +1,7 @@
 package jp.dip.prog;
 
 import com.intuit.karate.junit4.Karate;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
-// import test.ServerStart;
 
 @RunWith(Karate.class)
 public abstract class TestBase {
